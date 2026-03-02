5ae075ad4af47c8be513e423ddb008fd11a3e891
brian d foy <briandfoy@pobox.com>
2026-03-02 11:46:54 -0500

    finish the docs in Tie::*

--- lib/Tie/String/Redactable.pm
+++ lib/Tie/String/Redactable.pm
@@ -21,8 +21,6 @@ sub STORE {
 
 =encoding utf8
 
-=pod
-
 =head1 NAME
 
 Tie::String::Redactable - work even harder to redact a string
@@ -31,16 +29,37 @@ Tie::String::Redactable - work even harder to redact a string
 
 	use Tie::String::Redactable;
 
-	my $object = tie my $string, 'Tie::String::Redactable', $password;
+	my $object = tie my $string, 'Tie::String::Redactable', $secret;
 
-	# None of these return $password
-
-
-	#
 	$object->to_str_unsafe;
 	(tied $string)->to_str_unsafe;
 
 =head1 DESCRIPTION
+
+This is a tied version of L<String::Redactable> that refuses to store
+a new value, and you have to go through the tied object to get to the
+object that allows you to call the method you need. It's unlikely that
+you would inadvertently type out any of that.
+
+=head1 TO DO
+
+=head1 SEE ALSO
+
+=head1 SOURCE AVAILABILITY
+
+This source is on GitHub:
+
+	http://github.com/briandfoy/string-redactable
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2025-2026, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
 
 =cut
 
